34df8239247747c72f43654adbe32eb76d5f6f3a
Alexis Varela <alexisbec13@hotmail.com>
2021-01-07 09:12:44 -0600

    finished program

---
+++ bubble_sort.rb
@@ -0,0 +1,44 @@
+def bubble_sort(array)
+  n = array.length
+
+  loop do
+    swap = false
+
+    (n - 1).times do |i|
+      if array[i] > array [i + 1]
+        array[i], array[i + 1] = array[i + 1], array[i]
+        swap = true
+      end
+    end
+
+    break if not swap
+  end
+  return array
+end
+
+p bubble_sort([3, 4, 5, 2, 1])
+
+def bubble_sort_by(array)
+  n = array.length
+
+  loop do
+    swap = false
+
+    (n - 1).times do |i|
+      
+      comparation = yield array[i], array[i + 1]
+
+      if comparation.positive?
+        array[i], array[i + 1] = array[i + 1], array[i]
+        swap = true
+      end
+    end
+
+    break if not swap
+  end
+  p array
+end
+
+bubble_sort_by(["hi","hello","hey"]) do |left,right|
+  left.length - right.length
+end
